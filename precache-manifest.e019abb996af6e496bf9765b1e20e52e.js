2398aaa596054cdb5202e04f72fd0de9110beb60
akshayakky <akshaysmpt01@gmail.com>
2020-07-13 22:20:40 +0530

    Updates

---
+++ precache-manifest.e019abb996af6e496bf9765b1e20e52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd3860157eff977bc78fe0f7a5da6d0",
+    "url": "/quantity-converter/index.html"
+  },
+  {
+    "revision": "45aac572a9448b8343e1",
+    "url": "/quantity-converter/static/css/main.f4609f2f.chunk.css"
+  },
+  {
+    "revision": "ff2064d0ed90f7b68f64",
+    "url": "/quantity-converter/static/js/2.bb0b1ceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quantity-converter/static/js/2.bb0b1ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45aac572a9448b8343e1",
+    "url": "/quantity-converter/static/js/main.856c310f.chunk.js"
+  },
+  {
+    "revision": "9353340788ba8b486f67",
+    "url": "/quantity-converter/static/js/runtime-main.0a6b2db2.js"
+  }
+]);
